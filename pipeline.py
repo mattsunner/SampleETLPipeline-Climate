57f93b1393dd544f8c80df9c4ce9381c2a5346ed
Matthew Sunner <matt@mattsunner.com>
2022-11-27 21:05:04 -0600

    Create the pipeline.py File
    Create the base file for the simple ETL pipeline. Add in TODO reminders
    for the steps that need completed.

---
+++ pipeline.py
@@ -0,0 +1,20 @@
+"""
+pipeline.py - Entry point into the ETL pipeline.
+
+Author: Matthew Sunner, 2022
+"""
+
+import sqlite3
+
+import pandas as pd
+
+
+# TODO: Create a database connection to local sqlite file
+
+# TODO: Generate database schema using SQL file in current workdir
+
+# TODO: Read in raw csv data into the sqlite file with no alterations
+
+# TODO: Use Pandas to extract and transform the data into cleaned dataframes
+
+# TODO: Upload data into a cloud DB instance on AWS
